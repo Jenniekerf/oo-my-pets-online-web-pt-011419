beb5b881f5369343f9cfda170a7e6a023cb9a89f
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-13 17:53:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,7 +48,7 @@ pets.clear
   
   def list_pets
     
-    "I have #{pets[:fishes].count} fish, #{} dog(s), and #{} cat(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
   
   def name 
