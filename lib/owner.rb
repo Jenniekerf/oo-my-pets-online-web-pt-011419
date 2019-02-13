a53ab337461673d2edf4a7df9a38a0703027a487
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-13 17:00:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,20 @@
 class Owner
   
+  @@all = []
+  
   def initialize 
     
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset 
+    @@all.clear
   end
 end
